3df6b49bf84cded1e431e267af9101d8a38b575a
SwordJian <601827306@qq.com>
2017-10-27 14:55:07 +0800

    增加是否显示第一个对话框的设置方法

--- app/src/main/java/com/sword/permissionshelper/MainActivity.java
+++ app/src/main/java/com/sword/permissionshelper/MainActivity.java
@@ -2,6 +2,7 @@ package com.sword.permissionshelper;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -13,9 +14,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sword.permissionshelper.library.PermissionsHepler;
 
@@ -46,6 +52,47 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        LinearLayout scrollLayout = (LinearLayout) findViewById(R.id.scroll_layout);
+        for (int i = 0; i < 60; i++) {
+            LinearLayout textLayout = new LinearLayout(getApplicationContext());
+            textLayout.setPadding(0, 10, 0, 10);
+            scrollLayout.addView(textLayout, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 150));
+
+            TextView lTextView = new TextView(getApplicationContext());
+            lTextView.setText("i = " + (i + 1));
+            lTextView.setTag(i);
+            lTextView.setTextSize(20);
+            lTextView.setTextColor(Color.WHITE);
+            lTextView.setGravity(Gravity.CENTER);
+            lTextView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            textLayout.addView(lTextView, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+
+            lTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View pView) {
+                    int[] location = new int[2];
+                    pView.getLocationInWindow(location);
+                    int x = location[0];
+                    int y = location[1];
+
+                    Log.d("Sword", "pView-y-->" + y);
+                    Log.d("Sword", "pView-tag-->" + pView.getTag());
+                    Log.d("Sword", "-----------------------------");
+                    int[] location2 = new int[2];
+                    pView.getLocationOnScreen(location2);
+                    int x2 = location2[0];
+                    int y2 = location2[1];
+                    Log.d("Sword", "pView-y2-->" + y2);
+                    Log.d("Sword", "pView-tag-->" + pView.getTag());
+                    Log.d("Sword", "===============================");
+                }
+            });
+        }
+
+        Log.d("Sword", "has permission: " + PermissionsHepler.getInstance(this).checkPermissionGranted(Manifest.permission.CAMERA));
+        Log.d("Sword", "show permission: " + PermissionsHepler.getInstance(this).shouldShowRequestPermissionRationale(Manifest.permission.CAMERA));
+
     }
 
     @Override
@@ -93,13 +140,14 @@ public class MainActivity extends AppCompatActivity
                 public void hasPermission() {
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(intent, 1);
+                    Toast.makeText(getApplicationContext(), "获取权限成功", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void noPermission(String code) {
-
+                    Toast.makeText(getApplicationContext(), "获取权限失败: " + code, Toast.LENGTH_SHORT).show();
                 }
-            }, Manifest.permission.CAMERA);
+            }, true, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA);
 
         } else if (id == R.id.nav_gallery) {
 
@@ -120,8 +168,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (PermissionsHepler.getInstance(this).onRequestPermissionsResult(requestCode,permissions,grantResults))
-        {
+        if (PermissionsHepler.getInstance(this).onRequestPermissionsResult(requestCode, permissions, grantResults)) {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
--- permissionshelperlibrary/src/main/java/com/sword/permissionshelper/library/PermissionsHepler.java
+++ permissionshelperlibrary/src/main/java/com/sword/permissionshelper/library/PermissionsHepler.java
@@ -10,6 +10,7 @@ import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,21 +19,38 @@ import java.util.Map;
  * Created by SwordJian on 2016/12/2.
  *
  * @author SwordJian
- * @editor SwordJian on 2016/12/2
+ * @editor SwordJian on 2017/10/27
+ * <p>
+ * 封装Android M(6.0)以上的权限访问库，执行{@link #performCodeWithPermission(String, PermissionCallback, boolean, String...)}就可以弹出提示框。
+ * 在用户选择拒绝再次提示之后，会弹出Dialog来引导用户前往设置打开权限。
  */
 
 public class PermissionsHepler {
 
+    /**
+     * 授权后{@link #onRequestPermissionsResult(int, String[], int[])} 的回调Code
+     */
+    public static final int permissionRequestCode = 88;
+
     private Activity mActivity;
+
     private static PermissionsHepler sHepler;
 
+    /**
+     * 弹出的第一个Dialog文字
+     */
     private String permissionDes;
+    /**
+     * 弹出的第二个Dialog文字
+     */
     private String missPermissionDes;
-    private int permissionRequestCode = 88;
+    /**
+     * 是否显示权限提示
+     */
     public boolean isShowPermission = false;
     private PermissionCallback permissionCallback;
 
-    Map<String, Integer> permissionMap;
+    private Map<String, Integer> permissionMap;
 
     public interface PermissionCallback {
         void hasPermission();
@@ -43,9 +61,9 @@ public class PermissionsHepler {
     public static PermissionsHepler getInstance(Activity pAct) {
         if (sHepler == null) {
             sHepler = new PermissionsHepler();
+            sHepler.permissionMap = new HashMap<>();
         }
         sHepler.mActivity = pAct;
-        sHepler.permissionMap = new HashMap<>();
         return sHepler;
     }
 
@@ -64,6 +82,18 @@ public class PermissionsHepler {
      * @param permissions   请求的权限（数组类型），直接从Manifest中读取相应的值，比如Manifest.permission.WRITE_CONTACTS
      */
     public void performCodeWithPermission(@NonNull String permissionDes, PermissionCallback callback, @NonNull String... permissions) {
+        performCodeWithPermission(permissionDes, callback, false, permissions);
+    }
+
+    /**
+     * Android M运行时权限请求封装
+     *
+     * @param permissionDes 权限描述
+     * @param callback      请求权限回调
+     * @param isShowDialog  是否显示对话框
+     * @param permissions   请求的权限（数组类型），直接从Manifest中读取相应的值，比如Manifest.permission.WRITE_CONTACTS
+     */
+    public void performCodeWithPermission(@NonNull String permissionDes, PermissionCallback callback, boolean isShowDialog, @NonNull String... permissions) {
         isShowPermission = true;
         this.permissionDes = getActivity().getResources().getString(R.string.permission_tips);
         this.permissionDes = String.format(this.permissionDes, permissionDes);
@@ -73,14 +103,14 @@ public class PermissionsHepler {
 //        this.permissionrequestCode = requestCode;
         this.permissionCallback = callback;
 
-        if ((Build.VERSION.SDK_INT < Build.VERSION_CODES.M) || checkPermissionGranted(permissions)) {
+        if ((Build.VERSION.SDK_INT < Build.VERSION_CODES.M)/* || checkPermissionGranted(permissions)*/) {
             if (permissionCallback != null) {
                 permissionCallback.hasPermission();
                 permissionCallback = null;
             }
         } else {
             //permission has not been granted.
-            requestPermission(permissionRequestCode, permissions);
+            requestPermission(isShowDialog, permissionRequestCode, permissions);
         }
 
     }
@@ -107,10 +137,14 @@ public class PermissionsHepler {
     public boolean shouldShowRequestPermissionRationale(String... permissions) {
         boolean flag = false;
         for (String p : permissions) {
+            Log.d("Sword", p + " : " + ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), p));
+            Log.d("Sword", "---------------------------------");
             if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), p)) {
                 flag = true;
                 break;
             } else {
+                Log.d("Sword", p + " map : " + permissionMap.get(p));
+                Log.d("Sword", "---------------------------------");
                 if (permissionMap.get(p) == null || permissionMap.get(p) == 0) {
                     flag = true;
                     break;
@@ -120,16 +154,29 @@ public class PermissionsHepler {
         return flag;
     }
 
-    private void requestPermission(final int requestCode, final String[] permissions) {
+    private void requestPermission(boolean isShowDialog, final int requestCode, final String[] permissions) {
         if (shouldShowRequestPermissionRationale(permissions)) {
-            new AlertDialog.Builder(getActivity()).setMessage(permissionDes)
-                    .setPositiveButton(R.string.action_keepon, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-
-                            ActivityCompat.requestPermissions(getActivity(), permissions, requestCode);
+            if (isShowDialog) {
+                new AlertDialog.Builder(getActivity()).setMessage(permissionDes)
+                        .setPositiveButton(R.string.action_keepon, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(getActivity(), permissions, requestCode);
+                            }
+                        }).setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        if (permissionCallback != null) {
+                            permissionCallback.noPermission("cancel");
                         }
-                    }).show();
+                        isShowPermission = false;
+                        permissionCallback = null;
+                    }
+                }).show();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), permissions, requestCode);
+            }
+
         } else {
             new AlertDialog.Builder(getActivity()).setTitle(android.R.string.dialog_alert_title).setMessage(missPermissionDes)
                     .setPositiveButton(R.string.action_settings, new DialogInterface.OnClickListener() {
@@ -140,15 +187,18 @@ public class PermissionsHepler {
                             intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
                             intent.setData(Uri.fromParts("package", mActivity.getPackageName(), null));
                             mActivity.startActivity(intent);
-
-                            permissionCallback.noPermission("setting");
+                            if (permissionCallback != null) {
+                                permissionCallback.noPermission("setting");
+                            }
                             isShowPermission = false;
                             permissionCallback = null;
                         }
                     }).setNegativeButton(R.string.action_cancel, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    permissionCallback.noPermission("cancel");
+                    if (permissionCallback != null) {
+                        permissionCallback.noPermission("cancel");
+                    }
                     isShowPermission = false;
                     permissionCallback = null;
                 }
@@ -170,10 +220,13 @@ public class PermissionsHepler {
                 }
             } else {
                 if (permissionCallback != null) {
-                    permissionCallback.noPermission("");
+                    permissionCallback.noPermission("refuse");
 //                    permissionCallback = null;
                 }
                 for (String p : permissions) {
+
+                    Log.d("Sword", p + " refuse : " + permissionMap.get(p));
+                    Log.d("Sword", "---------------------------------");
                     permissionMap.put(p, 1);
                 }
             }
@@ -198,6 +251,14 @@ public class PermissionsHepler {
             }
         }
         return true;
+    }
+
+    public static String getVersion() {
+        return "1.0.5";
+    }
+
+    public static String getGitHubLink() {
+        return "https://github.com/SwordJian/PermissionsHelper";
     }
 
 }
